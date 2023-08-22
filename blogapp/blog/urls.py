2e1d6b35eb25e38ede955b1aa5fe546c2abe99d8
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-22 10:56:21 +0300

    Add dynamic link and data

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -9,10 +9,10 @@ from . import views
 # http://127.0.0.1:8000/blogs/3     => blog-details
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="home"),
     path("index", views.index),
     # Garip bir sekilde index'te sonuna ters slash eklemeden kabul ediyor ama
     # blogs kisminda hata veriyor.
-    path("blogs/", views.blogs),
-    path("blogs/<int:id>", views.blog_details),
+    path("blogs/", views.blogs, name="blogs"),
+    path("blogs/<int:id>", views.blog_details, name="blog_details"),
 ]
