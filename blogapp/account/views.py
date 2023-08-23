5b42a544e446943f6e5f8bb5c99df1e7c11fbbd4
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-23 17:22:14 +0300

    add register

--- blogapp/account/views.py
+++ blogapp/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -24,6 +25,51 @@ def login_request(request):
 
 
 def register_request(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", 
+                {
+                    "error": "username kullaniliyor.",
+                    "username":username,
+                    "email":email,
+                    "firstname":firstname,
+                    "lastname":lastname
+                })
+            else :
+                if User.objects.filter(email=email).exists():
+                    return render(request, "account/register.html", 
+                    {
+                        "error": "email kullaniliyor..",
+                        "username":username,
+                        "email":email,
+                        "firstname":firstname,
+                        "lastname":lastname
+                    })
+                else:
+                    user = User.objects.create_user(username=username,email=email,first_name=firstname,last_name= lastname, password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+            return render(request, "account/register.html", 
+            {
+                "error": "parola eslesmiyor.",
+                "username":username,
+                "email":email,
+                "firstname":firstname,
+                "lastname":lastname
+                
+            })
+        
+
     return render(request, "account/register.html")
 
 
