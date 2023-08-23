26b4cd1606817f43c262fc4092b3c9da71c7965c
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-23 15:21:49 +0300

    Update many to many

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -25,7 +25,7 @@ class Blog(models.Model):
     is_active = models.BooleanField()
     is_home = models.BooleanField(default=True) 
     slug = models.SlugField(null=False,blank=True, unique=True, db_index=True, editable=False)
-    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return f"{self.title}"
