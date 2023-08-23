46075fafca5acb737558afb7f108e34ba4a9ccc1
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-23 10:49:50 +0300

    Inital commit

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from blog.models import Blog
 
 
 
@@ -35,28 +36,21 @@ data = {
 # Create your views here.
 def index(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_home = True, is_active = True)
     }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_active = True)
     }
     return render(request, "blog/blogs.html", context)
 
 
 def blog_details(request, id):
-    # blogs = data["blogs"]
-    # selectedBlog = None
-
-    # for blog in blogs:
-    #     if blog["id"] == id:
-    #         selectedBlog = blog
-    blogs = data["blogs"]
-
-    selectedBlog = [blog for blog in blogs if blog["id"] == id][0]
+    
+    blog = Blog.objects.get(id = id)
     return render(request, "blog/blog-details.html", {
-        "blog": selectedBlog
+        "blog": blog
     })
